61e554a1aa312ae864dab36f58a77cdf0e20182f
cyberpirate92 <bsravi.tez@gmail.com>
2017-10-19 23:39:23 +0530

    Fixed #3

--- src/app/ng-table/ng-table.component.spec.ts
+++ src/app/ng-table/ng-table.component.spec.ts
@@ -3,23 +3,59 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgTableComponent } from './ng-table.component';
 
 describe('NgTableComponent', () => {
-  let component: NgTableComponent;
-  let fixture: ComponentFixture<NgTableComponent>;
+    let component: NgTableComponent;
+    let fixture: ComponentFixture<NgTableComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NgTableComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [NgTableComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NgTableComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
+});
+
+describe('getTitleCasedString() should return title cased strings', () => {
+    const testStrings = [
+        'firstName',
+        'lastName',
+        'first_name',
+        'first_Name',
+        'First Name',
+        'first Name',
+        'first*name',
+        'first__name',
+        'first name',
+        'first  name',
+        'firstname'
+    ];
+    const expectedResults = [
+        'First Name',
+        'Last Name',
+        'First Name',
+        'First Name',
+        'First Name',
+        'First Name',
+        'First*name',
+        'First Name',
+        'First Name',
+        'First Name',
+        'Firstname'
+    ];
+
+    it('should return title cased strings', () => {
+        expect(testStrings.length).toBe(expectedResults.length);
+        for (let i = 0; i < testStrings.length; i++) {
+            expect(NgTableComponent.getTitleCasedString(testStrings[i])).toBe(expectedResults[i]);
+        }
+    });
 });
--- src/app/ng-table/ng-table.component.ts
+++ src/app/ng-table/ng-table.component.ts
@@ -16,16 +16,67 @@ import {
 export class NgTableComponent implements OnInit, OnChanges {
 
     @Input() data: any[];
+    @Input() enableTitleCasedHeaders: boolean;
 
-    private columns: string[];
+    private columnHeaders: string[];
+    private properties: string[];
     private rows: string[][];
 
+    public static isLowerCase(x: string): boolean {
+        return x >= 'a' && x <= 'z';
+    }
+
+    public static isUpperCase(x: string): boolean {
+        return x >= 'A' && x <= 'Z';
+    }
+
+    public static isDigit(x: string): boolean {
+        return x >= '0' && x <= '9';
+    }
+
+    public static getTitleCasedString(s: string): string {
+        s = s.trim();
+        console.log('Before=> "' + s + '"');
+        let t = '';
+        if (s.length !== 0) {
+            if (NgTableComponent.isLowerCase(s[0])) {
+                t += s[0].toUpperCase();
+            } else {
+                t += s[0];
+            }
+            for (let i = 1; i < s.length; i++) {
+                if (NgTableComponent.isUpperCase(s[i])) {
+                    if (t.length !== 0 && t[t.length - 1] !== ' ') {
+                        t += ' ' + s[i];
+                    } else {
+                        t += s[i];
+                    }
+                } else if ( (s[i] === '_' || s[i] === ' ')) {
+                    if ( t.length !== 0 &&  t[t.length - 1] !== ' ') {
+                        t += ' ';
+                    }
+                } else if (NgTableComponent.isLowerCase(s[i])) {
+                    if ( t.length !== 0 &&  t[t.length - 1] === ' ' ) {
+                        t += s[i].toUpperCase();
+                    } else {
+                        t += s[i];
+                    }
+                } else {
+                    t += s[i];
+                }
+            }
+        }
+        console.log('After=> "' + t + '"');
+        return t;
+    }
+
     constructor() {
+        this.enableTitleCasedHeaders = false;
         this.clear();
     }
 
     public clear() {
-        this.columns = [];
+        this.columnHeaders = [];
         this.rows = [];
     }
 
@@ -41,21 +92,31 @@ export class NgTableComponent implements OnInit, OnChanges {
 
     extractRowsAndColumns(objArray: any[]): void {
         this.clear();
-        const colSet = new Set<string>();
+        const propSet = new Set<string>();
         for (let i = 0; i < objArray.length; i++) {
             const keys = Object.keys(objArray[i]);
             for (let j = 0; j < keys.length; j++) {
-                colSet.add(keys[j]);
+                propSet.add(keys[j]);
             }
         }
-        this.columns = Array.from(colSet);
+
+        this.properties = Array.from(propSet);
+
+        if (!this.enableTitleCasedHeaders) {
+            this.columnHeaders = this.properties;
+        } else {
+            const cols = this.properties.slice(0);
+            for (let i = 0; i < cols.length; i++) {
+                this.columnHeaders.push(NgTableComponent.getTitleCasedString(cols[i]));
+            }
+        }
 
         for (let i = 0; i < objArray.length; i++) {
             const obj = objArray[i];
             const row = [];
-            for (let j = 0; j < this.columns.length; j++) {
-                if (obj.hasOwnProperty(this.columns[j])) {
-                    row.push(obj[this.columns[j]]);
+            for (let j = 0; j < this.properties.length; j++) {
+                if (obj.hasOwnProperty(this.properties[j])) {
+                    row.push(obj[this.properties[j]]);
                 } else {
                     row.push(null);
                 }
